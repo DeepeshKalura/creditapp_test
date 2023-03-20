6255ad5411a5871be162a55bb39911fafe4828ea
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-20 08:46:39 +0530

    Basic Structure Has been build

---
+++ lib/screen/credit_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyCreditScreen extends StatefulWidget {
+  const MyCreditScreen({super.key});
+
+  @override
+  State<MyCreditScreen> createState() => _MyCreditScreenState();
+}
+
+class _MyCreditScreenState extends State<MyCreditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
